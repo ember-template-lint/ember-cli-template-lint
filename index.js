8959e555af23751c61a1609c12b09b04276869e6
Robert Jackson <robert.w.jackson@me.com>
2016-05-01 01:09:55 -0400

    Set minimum ember-cli version.

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 /*eslint-env node*/
 'use strict';
 
+var VersionChecker = require('ember-cli-version-checker');
 var TemplateLinter = require('./broccoli-template-linter');
 
 module.exports = {
   name: 'ember-cli-template-lint',
 
   lintTree: function(type, tree) {
+    var checker = new VersionChecker(this);
+    checker.for('ember-cli', 'npm').assertAbove('2.4.1');
+
     if (type === 'templates') {
       var ui = this.ui;
       var mockConsole = {
