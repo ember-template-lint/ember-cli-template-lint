ba1b1ddf5181e78d3220c5e3d1d880bcf775c9ea
Robert Jackson <robert.w.jackson@me.com>
2016-04-30 23:37:27 -0400

    Use the recommended config from ember-template-lint.

--- blueprints/ember-cli-template-lint/files/.template-lintrc.js
+++ blueprints/ember-cli-template-lint/files/.template-lintrc.js
@@ -2,11 +2,5 @@
 'use strict';
 
 module.exports = {
-  'bare-strings': true,
-  'block-indentation': 2,
-  'html-comments': true,
-  'nested-interactive': true,
-  'lint-self-closing-void-elements': true,
-  'triple-curlies': true,
-  'deprecated-each-syntax': true
+  extend: 'recommended'
 };
